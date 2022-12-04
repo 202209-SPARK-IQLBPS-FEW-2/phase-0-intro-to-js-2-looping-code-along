f544efc968acbd0dbfd6bee745c1f74c70406515
roza <rozanawzad2001@gmail.com>
2022-12-04 17:46:39 +0300

    done js looping code lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    const messages=[];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+      return messages;
+  }
+  writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+  
+function countDown(number){
+    while(number>=0){
+        console.log(number--);
+        //number --;
+    }
+}
+  
+
+ 
